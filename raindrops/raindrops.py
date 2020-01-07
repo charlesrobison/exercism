68481afd2e7395ffd9fb846cf9673b11eea139b6
charlesrobison <crobison@uw.edu>
2020-01-06 21:54:26 -0800

    simplify using concat per feedback

--- raindrops/raindrops.py
+++ raindrops/raindrops.py
@@ -1,17 +1,11 @@
 def convert(number):
-    if number % (7*5*3) == 0:
-        return 'PlingPlangPlong'
-    elif number % (7*5) == 0:
-        return 'PlangPlong'
-    elif number % (7*3) == 0:
-        return 'PlingPlong'
-    elif number % (5*3) == 0:
-        return 'PlingPlang'
-    elif number % 7 == 0:
-        return 'Plong'
-    elif number % 5 == 0:
-        return 'Plang'
-    elif number % 3 == 0:
-        return 'Pling'
-    else:
-        return str(number)
+    raindrops = ''
+    if number % 3 == 0:
+        raindrops += 'Pling'
+    if number % 5 == 0:
+        raindrops += 'Plang'
+    if number % 7 == 0:
+        raindrops += 'Plong'
+    if number % 3 != 0 and number % 5 != 0 and number % 7 != 0:
+        raindrops = str(number)
+    return raindrops
